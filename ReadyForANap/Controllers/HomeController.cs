5fdf60f1ae198051a3c14fd95cbc704d1b8a1532
kihy0804 <kihy0804@gmail.com>
2018-10-25 15:39:17 -0600

    first commit

---
+++ ReadyForANap/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using ReadyForANap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReadyForANap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Team> listTeam = new List<Team>();
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Scaffolding --> Work on building something, make things more flexible
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Team myTeam)
+        {
+            //Making IDs using the data the user enters for TeamDescription
+            myTeam.TeamID = myTeam.TeamDescription.Substring(0, 2) + " NUM# " + listTeam.Count().ToString();
+            //Add the data to the object
+            listTeam.Add(myTeam);
+
+            return View();
+        }
+    }
+}
---
+++ ReadyForANap/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ReadyForANap.Models
+{
+    public class Team
+    {
+        //Make sure you build before you do anything, to make your model visible
+        //When you Build Solution, you're basically COMPILE your codes, making sure you're syntactically correct (syntax)
+        //Store Data --> Purpose of Model
+
+        [DisplayName("Team ID")]
+        public string TeamID { get; set; }
+        //.NET automatically thinks anything that contains "ID" is the primary key! (Last two characters, which is "ID" in this case)
+        //We don't normally want people to edit our Primary Key, so it is not showing up in our index view but Team Name does!
+
+        [DisplayName("Team Name")]
+        public string TeamDescription { get; set; }
+    }
+}
